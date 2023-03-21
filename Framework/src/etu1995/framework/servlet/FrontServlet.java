2bed1090c09b2b557d9511eed69a029e1866e33b
dkasrtg <kelydoda724@gmail.com>
2023-03-21 06:13:06 +0300

    Added TestFramework project and Modified repostory structure

--- Framework/src/etu1995/framework/servlet/FrontServlet.java
+++ Framework/src/etu1995/framework/servlet/FrontServlet.java
@@ -4,18 +4,15 @@ import annotations.MappingUrl;
 import etu1995.framework.Mapping;
 import org.apache.commons.io.FileUtils;
 
-import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
+import javax.servlet.*;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.lang.reflect.Method;
-import java.net.URL;
 import java.util.Collection;
+import java.util.Enumeration;
 import java.util.HashMap;
 
 public class FrontServlet extends HttpServlet{
@@ -25,7 +22,7 @@ public class FrontServlet extends HttpServlet{
     public void init(ServletConfig config) throws ServletException {
         super.init(config);
         try {
-            setMappingUrls(new HashMap<>());
+            setMappingUrls(new HashMap<>(),getInitParameter("path"));
             System.out.println(getMappingUrls());
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -49,30 +46,40 @@ public class FrontServlet extends HttpServlet{
             throw new RuntimeException(e);
         }
     }
+
     public void processRequest(HttpServletRequest req,HttpServletResponse resp) throws Exception {
         String url = req.getRequestURL().toString();
-        if(!req.getQueryString().isEmpty()){
-            url = url + "?" + req.getQueryString();
+        try {
+            if (!req.getQueryString().isEmpty()) {
+                url = url + "?" + req.getQueryString();
+            }
+        }catch (Exception ignored){}
+        System.out.println(url);
+        Enumeration<String> parameters = req.getParameterNames();
+        System.out.println("Parameters:");
+        while (parameters.hasMoreElements()){
+            String pname = parameters.nextElement();
+            System.out.println(pname+": "+req.getParameter(pname));
         }
-        req.setAttribute("url",url);
-        RequestDispatcher requestDispatcher = req.getRequestDispatcher("frontservlet.jsp");
-        requestDispatcher.forward(req,resp);
     }
+
 
     public HashMap<String, Mapping> getMappingUrls() {
         return MappingUrls;
     }
 
-    public void setMappingUrls(HashMap<String, Mapping> mappingUrls) throws Exception {
+    public void setMappingUrls(HashMap<String, Mapping> mappingUrls,String pat) throws Exception {
         MappingUrls = mappingUrls;
-        File f = new File(getServletContext().getRealPath("/strs.properties").replaceAll("strs.properties",""));
-        Collection<File> files = FileUtils.listFiles(f,new String[]{"class"},true);
+        File f = new File("C:\\xampp\\tomcat\\webapps\\WDN2\\src\\types");
+        Collection<File> files = FileUtils.listFiles(f,new String[]{"java"},true);
+        int ind = f.getAbsolutePath().lastIndexOf("\\");
+        String pack = f.getAbsolutePath().substring(ind+1);
         for (File p :files){
             String path = p.getAbsolutePath();
-            path = path.split("classes")[1];
-            path = path.substring(path.indexOf('\\')+1);
-            path = path.replace('\\','.');
-            path = path.replaceAll(".class","");
+            path = path.split(pack)[1];
+            path = pack +path;
+            path = path.replaceAll(".java","");
+            path = path.replace("\\",".");
             Class<?> cls = Class.forName(path);
             for (Method m : cls.getDeclaredMethods()){
                 if (m.isAnnotationPresent(MappingUrl.class)){
