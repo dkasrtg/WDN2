c80214c845b5aef79da680916f8c4ddc1ce998db
dkasrtg <kelydoda724@gmail.com>
2023-03-22 11:51:54 +0300

    Pushing new files failed

---
+++ Framework/build.bat
@@ -0,0 +1,14 @@
+mkdir temp
+cd src
+javac -d ../temp/ ./annotations/MappingUrl.java
+javac -d ../temp/ ./etu1995/framework/Mapping.java
+javac -d ../temp/ ./etu1995/framework/servlet/FrontServlet.java
+cd ../temp
+jar cvf framework.jar *
+cp framework.jar ../../TestFramework/WEB-INF/lib/framework.jar
+cd ../
+rm -R temp
+cd ../TestFramework
+jar cvf TestFramework.war *
+cp TestFramework.war C:/xampp/tomcat/webapps/TestFramework.war
+cd ../Framework
