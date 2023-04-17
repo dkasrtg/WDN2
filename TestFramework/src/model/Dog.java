abd3331c14cc734c65a503d1c03cbe4205459b2a
dkasrtg <kelydoda724@gmail.com>
2023-04-17 14:32:00 +0300

    Added functionality to get data from url and retrieve it from a model function

--- TestFramework/src/model/Dog.java
+++ TestFramework/src/model/Dog.java
@@ -3,8 +3,11 @@ package model;
 import annotations.MappingUrl;
 import etu1995.framework.ModelView;
 
+import java.util.HashMap;
+
 public class Dog {
     String name;
+
 
     public Dog(){
 
@@ -29,4 +32,14 @@ public class Dog {
         modelView.addItem("alik",new Dog("Alik"));
         return modelView;
     }
+
+    @MappingUrl(url = "dog-dogger")
+    public void dogme(HashMap<String,String> parameters){
+        System.out.println("Dogger");
+        parameters.forEach(
+                (key,value)
+                        -> System.out.println(key + " : " + value)
+        );
+    }
+
 }
